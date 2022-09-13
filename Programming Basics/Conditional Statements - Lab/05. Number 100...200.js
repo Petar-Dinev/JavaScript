9f2bad374756babd07c5891364e0887d065d5c2b
Petar-Dinev <p.dinev55@gmail.com>
2022-09-13 20:38:37 +0300

    add 2 more tasks from basic

---
+++ Programming Basics/Conditional Statements - Lab/05. Number 100...200.js
@@ -0,0 +1,11 @@
+function numberBetween100And200(input) {
+    let number = Number(input[0]);
+
+    if(number < 100) {
+        console.log("Less than 100");
+    } else if (number > 200) {
+        console.log("Greater than 200");
+    } else {
+        console.log("Between 100 and 200");
+    }
+}
