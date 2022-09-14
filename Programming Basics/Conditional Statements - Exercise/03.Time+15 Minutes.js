23815dd033c1b11818a349388fe80b7a518b4d3e
Petar-Dinev <p.dinev55@gmail.com>
2022-09-14 20:14:03 +0300

    add three tasks from basic

---
+++ Programming Basics/Conditional Statements - Exercise/03.Time+15 Minutes.js
@@ -0,0 +1,23 @@
+function timePlus15Minutes(input) {
+  let hours = Number(input[0]);
+  let minutes = Number(input[1]);
+
+  minutes += 15;
+
+  if (minutes >= 60) {
+    hours += 1;
+    minutes -= 60;
+  }
+
+  if (hours >= 24) {
+    hours = 0;
+  }
+  
+  if (minutes < 10) {
+    console.log(`${hours}:0${minutes}`);
+  } else {
+    console.log(`${hours}:${minutes}`);
+  }
+}
+
+timePlus15Minutes(["23", "59"]);
