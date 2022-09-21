4d0faeebd2d4267eb9fdb3ad666be83f25ad94ec
Petar <112411388+Petar-Dinev@users.noreply.github.com>
2022-09-21 21:03:48 +0300

    fix some problems in tasks

--- JS Advanced - Exercises/Arrays and Nested Arrays - Exercise/05. Extract Increasing Subsequence from Array.js
+++ JS Advanced - Exercises/Arrays and Nested Arrays - Exercise/05. Extract Increasing Subsequence from Array.js
@@ -10,7 +10,7 @@ function extractIncreasingSubsequenceFromArray(numbers) {
     }
   }
 
-  console.log(result);
+  return result;
 }
 
 extractIncreasingSubsequenceFromArray([1, 3, 8, 4, 10, 12, 3, 2, 24]);
