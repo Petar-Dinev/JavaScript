2cb9c0ea4f9c2a510592e6a6516c05fc3310f61d
Petar Dinev <p.dinev55@gmail.com>
2022-10-04 22:44:33 +0300

    fix the task from fundamentals

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers.js
@@ -0,0 +1,30 @@
+function sortNumbers(num1, num2, num3) {
+  if (num1 > num2 && num1 > num3) {
+    console.log(num1);
+    if (num2 > num3) {
+      console.log(num2);
+      console.log(num3);
+    } else {
+      console.log(num3);
+      console.log(num2);
+    }
+  } else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+    if (num1 > num3) {
+      console.log(num1);
+      console.log(num3);
+    } else {
+      console.log(num3);
+      console.log(num1);
+    }
+  } else if (num3 > num1 && num3 > num2) {
+    console.log(num3);
+    if (num2 > num1) {
+      console.log(num2);
+      console.log(num1);
+    } else {
+      console.log(num1);
+      console.log(num2);
+    }
+  }
+}
