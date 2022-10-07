1e84cde7321d9fac5d00fab7c33406249af9aff9
Petar Dinev <p.dinev55@gmail.com>
2022-10-07 23:22:25 +0300

    add 2 tasks from basic

---
+++ Programming Basics/For Loop - Exercise/02. Multiplication Table.js
@@ -0,0 +1,10 @@
+function multiplicationTable(input) {
+  let num = Number(input[0]);
+
+  for (let i = 1; i <= 10; i++) {
+    let res = i * num;
+    console.log(`${i} * ${num} = ${res}`);
+  }
+}
+
+multiplicationTable("5")
