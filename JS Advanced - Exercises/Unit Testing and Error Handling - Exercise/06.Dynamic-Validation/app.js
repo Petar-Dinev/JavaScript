1f9c035c98dd67f4afa4f0a6e0570b5d2b5655c1
Petar-Dinev <p.dinev55@gmail.com>
2022-10-18 23:18:32 +0300

    add one task from js-advanced module

---
+++ JS Advanced - Exercises/Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -0,0 +1,12 @@
+function validate() {
+  let emailEl = document.getElementById("email");
+  let pattern = new RegExp('^[a-z]+@[a-z]+\\.[a-z]+');
+
+  emailEl.addEventListener("change", () => {
+    if (pattern.test(emailEl.value)) {
+      emailEl.classList.remove("error");
+    } else {
+      emailEl.classList.add("error");
+    }
+  });
+}
