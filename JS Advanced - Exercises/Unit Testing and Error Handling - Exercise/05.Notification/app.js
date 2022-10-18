28770f90ab808821366b1f2eb0b76b0e5b29bf81
Petar-Dinev <p.dinev55@gmail.com>
2022-10-18 23:19:06 +0300

    add one more task from advanced module

---
+++ JS Advanced - Exercises/Unit Testing and Error Handling - Exercise/05.Notification/app.js
@@ -0,0 +1,9 @@
+function notify(message) {
+  let divEl = document.getElementById("notification");
+  divEl.textContent = message;
+  divEl.style.display = "block";
+
+  divEl.addEventListener("click", () => {
+    divEl.style.display = "none";
+  });
+}
