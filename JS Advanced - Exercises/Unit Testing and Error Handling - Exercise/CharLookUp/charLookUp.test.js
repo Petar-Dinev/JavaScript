4b1759ba5059edb35de9b9de3de2bfa28e55e635
Petar-Dinev <p.dinev55@gmail.com>
2022-10-18 23:17:51 +0300

    add one task from js-advanced

---
+++ JS Advanced - Exercises/Unit Testing and Error Handling - Exercise/CharLookUp/charLookUp.test.js
@@ -0,0 +1,23 @@
+let { expect } = require("chai");
+let { lookupChar } = require("../03. Char Lookup");
+
+describe("test lookupChar functionality", () => {
+  it("should return undefined with no string first param", () => {
+    expect(lookupChar(5, 5)).to.be.undefined;
+  });
+  it("should return undefined if second param is not intiger", () => {
+    expect(lookupChar("Pesho", "hi")).to.be.undefined;
+  });
+  it("should return undefined if second param is float point", () => {
+    expect(lookupChar("Pesho", 0.1)).to.be.undefined;
+  });
+  it("scheck valuer with index bigger from string length", () => {
+    expect(lookupChar("Pesho", 10)).to.equal("Incorrect index");
+  });
+  it("check valuer with negative index", () => {
+    expect(lookupChar("Pesho", -10)).to.equal("Incorrect index");
+  });
+  it("check value with valid params", () => {
+    expect(lookupChar("Pesho", 1)).to.equal("e");
+  });
+});
