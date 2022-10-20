667d8880900626fb610dd2fedd6c83f5b9bfbe96
Petar-Dinev <p.dinev55@gmail.com>
2022-10-20 21:01:08 +0300

    add one task from js-advanced

---
+++ JS Advanced - Exercises/Classes and Attributes - Exercise/02. Data Class.js
@@ -0,0 +1,15 @@
+class Request {
+  method;
+  uri;
+  version;
+  message;
+  response = undefined;
+  fulfilled = false;
+
+  constructor(method, uri, version, message) {
+    this.method = method;
+    this.uri = uri;
+    this.version = version;
+    this.message = message;
+  }
+}
