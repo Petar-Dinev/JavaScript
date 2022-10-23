c6a829a1f75584b6b27620f14a3a73b6102627b8
Petar-Dinev <p.dinev55@gmail.com>
2022-10-23 22:48:08 +0300

    add one task from js-advanced module

---
+++ Programming Basics/Conditional Statements Advanced - Lab/07.Working Hours.js
@@ -0,0 +1,21 @@
+function workingHours(input) {
+  const hour = Number(input[0]);
+  const day = input[1];
+
+  switch (day) {
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+    case "Saturday":
+      if (hour >= 10 && hour <= 18) {
+        console.log("open");
+      } else {
+        console.log("closed");
+      }
+      break;
+    case "Sunday":
+      console.log("closed");
+  }
+}
