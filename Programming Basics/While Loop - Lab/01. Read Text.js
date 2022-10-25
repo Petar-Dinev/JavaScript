9fcf41a19135bf79587955dcce116ef84c614c27
Petar-Dinev <p.dinev55@gmail.com>
2022-10-25 21:10:34 +0300

    add one task from basic module

---
+++ Programming Basics/While Loop - Lab/01. Read Text.js
@@ -0,0 +1,10 @@
+function readText(input) {
+  let index = 0;
+  let text = input[index];
+  index++;
+  while (text !== "Stop") {
+    console.log(text);
+    text = input[index];
+    index++;
+  }
+}
