1e9b673b8602ca509124a3634017f4205b514a88
Petar-Dinev <p.dinev55@gmail.com>
2022-10-30 20:55:36 +0200

    add one task from fundamentals

---
+++ Fundamentals/Data Types and Variables - More Exercise/01. Digits with Words.js
@@ -0,0 +1,38 @@
+function digitsWithWords(str) {
+  let result;
+
+  switch (str) {
+    case "one":
+      result = 1;
+      break;
+    case "two":
+      result = 2;
+      break;
+    case "three":
+      result = 3;
+      break;
+    case "four":
+      result = 4;
+      break;
+    case "five":
+      result = 5;
+      break;
+    case "six":
+      result = 6;
+      break;
+    case "seven":
+      result = 7;
+      break;
+    case "eight":
+      result = 8;
+      break;
+    case "nine":
+      result = 9;
+      break;
+    case "zero":
+      result = 0;
+      break;
+  }
+
+  console.log(result);
+}
