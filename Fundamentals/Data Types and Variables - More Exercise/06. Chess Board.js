a3e2de9828a4e9da594970369f40d71fc1b5e154
Petar-Dinev <p.dinev55@gmail.com>
2022-11-15 23:58:55 +0200

    add one task from fundamentals

---
+++ Fundamentals/Data Types and Variables - More Exercise/06. Chess Board.js
@@ -0,0 +1,28 @@
+function chessBoard(num) {
+  
+  let result = '<div class="chessboard">';
+  for (let i = 0; i < num; i++) {
+    result += "\n" + "  <div>";
+    for (let k = 0; k < num; k++) {
+      if (i % 2 != 0) {
+        if (k % 2 != 0) {
+          result += "\n" + '    <span class="black"></span>';
+        } else {
+          result += "\n" + '    <span class="white"></span>';
+        }
+      } else {
+        if (k % 2 != 0) {
+          result += "\n" + '    <span class="white"></span>';
+        } else {
+          result += "\n" + '    <span class="black"></span>';
+        }
+      }
+    
+    }
+    result += "\n" + "  </div>";
+  }
+  result += "\n" + "</div>";
+  console.log(result);
+}
+
+chessBoard(6);
