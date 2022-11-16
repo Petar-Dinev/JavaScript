542eec6c0674d7b1831d777000d8c47cea8e5180
Petar-Dinev <p.dinev55@gmail.com>
2022-11-16 21:20:27 +0200

    fix few problems in this tasks from js-application

--- JS Aplications - Exercies/Data and Authentication - Exercise/02.Phonebook/app.js
+++ JS Aplications - Exercies/Data and Authentication - Exercise/02.Phonebook/app.js
@@ -1,52 +1,59 @@
 function attachEvents() {
-    document.getElementById("btnLoad").addEventListener("click", onLoad);
-    document.getElementById("btnCreate").addEventListener("click", createContact)
+  document.getElementById("btnLoad").addEventListener("click", onLoad);
+  document.getElementById("btnCreate").addEventListener("click", createContact);
 }
 
 async function onLoad() {
-    const list = document.getElementById("phonebook");
-    const response = await fetch("http://localhost:3030/jsonstore/phonebook");
-    const data = await response.json();
-    const contacts = Array.from(Object.values(data))
-    list.replaceChildren(...contacts.map(createLiEl))
+  const list = document.getElementById("phonebook");
+  const response = await fetch("http://localhost:3030/jsonstore/phonebook");
+  const data = await response.json();
+  const contacts = Array.from(Object.values(data));
+  list.innerHTML = "";
+  contacts.forEach((obj) => {
+    list.appendChild(createLiEl(obj));
+  });
 }
 
 async function createContact() {
-   const personInput = document.getElementById("person");
-   const phoneInput = document.getElementById("phone");
-   const person = personInput.value;
-   const phone = phoneInput.value;
+  const personInput = document.getElementById("person");
+  const phoneInput = document.getElementById("phone");
+  const person = personInput.value;
+  const phone = phoneInput.value;
 
-   const response = await fetch("http://localhost:3030/jsonstore/phonebook", {
+  const response = await fetch("http://localhost:3030/jsonstore/phonebook", {
     method: "post",
     headers: {
-        "Content-Type": "aplication/json",
+      "Content-Type": "aplication/json",
     },
-    body: JSON.stringify({person, phone})
-   })
+    body: JSON.stringify({ person, phone }),
+  });
 
-   personInput.value = "";
-   phoneInput.value = "";
-   onLoad()
+  personInput.value = "";
+  phoneInput.value = "";
+
+  onLoad();
 }
 
 function createLiEl(obj) {
-    const liEl = document.createElement("li");
-    liEl.textContent = `${obj.person}: ${obj.phone}`
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "delete"
-    deleteBtn.addEventListener("click", onDelete)
-    liEl.appendChild(deleteBtn);
-    liEl.id = obj._id;
-    return liEl;
+  const liEl = document.createElement("li");
+  liEl.textContent = `${obj.person}: ${obj.phone}`;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", onDelete);
+  liEl.appendChild(deleteBtn);
+  liEl.id = obj._id;
+  return liEl;
 }
 
 async function onDelete(e) {
-   const id = e.target.parentElement.id
-
-   const response = await fetch("http://localhost:3030/jsonstore/phonebook" + id, {
-    method: "delete",
-   })
+  const id = e.target.parentElement.id;
+  const response = await fetch(
+    "http://localhost:3030/jsonstore/phonebook/" + id,
+    {
+      method: "delete",
+    }
+  );
+  e.target.parentElement.remove();
 }
 
 attachEvents();
--- JS Aplications - Exercies/Data and Authentication - Exercise/03.Students/app.js
+++ JS Aplications - Exercies/Data and Authentication - Exercise/03.Students/app.js
@@ -1,35 +1,49 @@
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 form.addEventListener("submit", onSubmit);
 const tBody = document.querySelector("tbody");
 
-onload()
-
 function onSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries())
-    const student = createTr(data);
-   
-    tBody.appendChild(student)
-    
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+  const student = createTr(data);
+  createStudent(data);
+  tBody.appendChild(student);
+}
+
+async function createStudent(data) {
+  const response = await fetch(
+    "http://localhost:3030/jsonstore/collections/students",
+    {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
+  );
 }
 
 async function onload() {
-   const response = await fetch("http://localhost:3030/jsonstore/collections/students");
-   const data = await response.json();
-   const students = Array.from(Object.values(data));
-   students.forEach(obj => {
-    tBody.appendChild(createTr(obj))
- })
+  const response = await fetch(
+    "http://localhost:3030/jsonstore/collections/students"
+  );
+  const data = await response.json();
+  const students = Array.from(Object.values(data));
+  students.forEach((obj) => {
+    tBody.appendChild(createTr(obj));
+  });
 }
-
 
 function createTr(obj) {
-   const trEl = document.createElement("tr");
-   trEl.innerHTML = `<td>${obj.firstName}</td>
+  const trEl = document.createElement("tr");
+  trEl.innerHTML = `<td>${obj.firstName}</td>
 <td>${obj.lastName}</td>
 <td>${obj.facultyNumber}</td>
-<td>${obj.grade}</td>`
+<td>${obj.grade}</td>`;
 
-   return trEl;
+  return trEl;
 }
+
+onload();
