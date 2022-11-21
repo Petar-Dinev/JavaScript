f7bfa93732f1b965ef2c98a5ad8cd8ffaa82282b
Petar-Dinev <p.dinev55@gmail.com>
2022-11-21 21:35:17 +0200

    add one task from JS-basic

---
+++ Programming Basics/Nested Loops - Lab/02. Multiplication Table.js
@@ -0,0 +1,9 @@
+function multiplicationTable() {
+  for (let i = 1; i <= 10; i++) {
+    for (let k = 1; k <= 10; k++) {
+      console.log(`${i} * ${k} = ${i * k}`);
+    }
+  }
+}
+
+multiplicationTable();
