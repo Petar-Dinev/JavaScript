70386422793107bf7b947d0a5cbb5aa60c7cab1b
Petar-Dinev <p.dinev55@gmail.com>
2023-03-15 23:35:35 +0200

    Add one task from JS Advanced

---
+++ JS Advanced - Exercises/Advanced Functions - Lab/02. Add.js
@@ -0,0 +1,12 @@
+function solution(num) {
+   return (arg) => arg + num
+}
+
+// let add5 = solution(5);
+// console.log(add5(2));
+// console.log(add5(3));
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
+
