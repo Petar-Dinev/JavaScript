21d15712b1f8b2c97e84fafd525fca37888215f5
Petar-Dinev <p.dinev55@gmail.com>
2023-12-16 22:07:00 +0200

    Update  one task from JS Advanced module

--- JS Advanced - Exercises/Exams/JS Advanced Exam - 21 Oct 2023/02.Flight Booking System/FlightBookingSystem.js
+++ JS Advanced - Exercises/Exams/JS Advanced Exam - 21 Oct 2023/02.Flight Booking System/FlightBookingSystem.js
@@ -21,36 +21,72 @@ class FlightBookingSystem {
     if (!flight) {
       return `Flight ${flightNumber} is not available for booking.`;
     } else {
-      this.bookings.push({passengerName, flightNumber});
+      this.bookings.push({ passengerName, flightNumber });
       this.bookingsCount += 1;
       return `Booking for passenger ${passengerName} on flight ${flightNumber} is confirmed.`;
     }
   }
-  cancelBooking (passengerName, flightNumber) {
-    let passenger = this.bookings.find(x => (x.passengerName == passengerName && x.flightNumber == flightNumber))
-    if(passenger) {
-        this.bookingsCount -= 1;
-        this.bookings.slice(this.bookings.findIndex(x => x.passengerName == passengerName) , 1)
-        return `Booking for passenger ${passengerName} on flight ${flightNumber} is cancelled.`
+  cancelBooking(passengerName, flightNumber) {
+    let passenger = this.bookings.find(
+      (x) => x.passengerName == passengerName && x.flightNumber == flightNumber
+    );
+    if (passenger) {
+      this.bookingsCount -= 1;
+      this.bookings.slice(
+        this.bookings.findIndex((x) => x.passengerName == passengerName),
+        1
+      );
+      return `Booking for passenger ${passengerName} on flight ${flightNumber} is cancelled.`;
     } else {
-        return `Booking for passenger ${passengerName} on flight ${flightNumber} not found.`
+      throw new Error(`Booking for passenger ${passengerName} on flight ${flightNumber} not found.`);
     }
   }
-  showBookings (criteria) {
-    if(this.bookings.length == 0) {
-        throw new Error(`No bookings have been made yet.`)
+  showBookings(criteria) {
+    if (this.bookings.length == 0) {
+      throw new Error(`No bookings have been made yet.`);
     }
 
     let result = [];
     let flights = [];
 
-    if(criteria == 'all') {
-        result.push(`All bookings(${this.bookingsCount}):`)
-        flights = this.bookings
-    } else if(criteria == 'cheap') {
-    
+    if (criteria == "all") {
+      result.push(`All bookings(${this.bookingsCount}):`);
+      flights = this.flights;
+    } else if (criteria == "cheap") {
+      result.push("Cheap bookings:");
+      flights = this.flights.filter((x) => x.price <= 100);
+      if (flights.length == 0) {
+        return "No cheap bookings found.";
+      }
     } else {
-
+      result.push("Expensive bookings:");
+      flights = this.flights.filter((x) => x.price > 100);
+      if (flights.length == 0) {
+        return "No expensive bookings found.";
+      }
     }
+
+    for (let flight of flights) {
+      this.bookings
+        .filter((b) => b.flightNumber == flight.flightNumber)
+        .map((b) =>
+          result.push(`${b.passengerName} booked for flight ${b.flightNumber}.`)
+        );
+    }
+
+    return result.join('\n')
   }
 }
+
+const system = new FlightBookingSystem("TravelWorld");
+  console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+  console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
+  console.log(system.bookFlight("Alice", "AA101"));
+  console.log(system.bookFlight("Bob", "BB202"));
+  console.log(system.showBookings("expensive"));
+  console.log(system.showBookings("cheap"));
+
+
+
+
+
