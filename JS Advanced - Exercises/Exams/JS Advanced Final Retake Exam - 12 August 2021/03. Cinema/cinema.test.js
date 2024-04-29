7f4adaac861b9ffa35d43760f055e9dfc16346aa
Petar-Dinev <p.dinev55@gmail.com>
2024-04-29 10:49:05 +0300

    Add one task from JS Advanced module

--- JS Advanced - Exercises/Exams/JS Advanced Final Retake Exam - 12 August 2021/03. Cinema/cinema.test.js
+++ JS Advanced - Exercises/Exams/JS Advanced Final Retake Exam - 12 August 2021/03. Cinema/cinema.test.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai')
 const cinema = require('./cinema')
 
-describe('Cinema tests', function() {
-    describe('showMovies method tests', function() {
+describe('Cinema tests', function () {
+    describe('showMovies method tests', function () {
         it('should return correct value with empty array for param', () => {
             expect(cinema.showMovies([])).to.be.equal("There are currently no movies to show.")
         })
@@ -11,11 +11,12 @@ describe('Cinema tests', function() {
             expect(cinema.showMovies(['a'])).to.be.equal('a')
         })
     })
-    describe('ticketPrice method tests', function() {
+    describe('ticketPrice method tests', function () {
         it('should throw error with invalid param', () => {
-           expect(() => cinema.ticketPrice('a')).to.throw("Invalid projection type.")
-           expect(() => cinema.ticketPrice('premiere')).to.throw("Invalid projection type.")
-           expect(() => cinema.ticketPrice('normal')).to.throw("Invalid projection type.")
+            expect(() => cinema.ticketPrice('a')).to.throw("Invalid projection type.")
+            expect(() => cinema.ticketPrice('premiere')).to.throw("Invalid projection type.")
+            expect(() => cinema.ticketPrice('normal')).to.throw("Invalid projection type.")
+            expect(() => cinema.ticketPrice('discount')).to.throw("Invalid projection type.")
         })
         it('should return correct price with valid param', () => {
             expect(cinema.ticketPrice("Premiere")).to.be.equal(12.00)
@@ -23,7 +24,7 @@ describe('Cinema tests', function() {
             expect(cinema.ticketPrice("Discount")).to.be.equal(5.50)
         })
     })
-    describe('swapSeatsInHall method tests', function() {
+    describe('swapSeatsInHall method tests', function () {
         it('should return correct string with invalid param', () => {
             expect(cinema.swapSeatsInHall(-1, 1)).to.be.equal("Unsuccessful change of seats in the hall.")
             expect(cinema.swapSeatsInHall(1, -1)).to.be.equal("Unsuccessful change of seats in the hall.")
@@ -38,15 +39,18 @@ describe('Cinema tests', function() {
             expect(cinema.swapSeatsInHall(1, 21)).to.be.equal("Unsuccessful change of seats in the hall.")
             expect(cinema.swapSeatsInHall(21, 1)).to.be.equal("Unsuccessful change of seats in the hall.")
             expect(cinema.swapSeatsInHall(21, 22)).to.be.equal("Unsuccessful change of seats in the hall.")
-            expect(cinema.swapSeatsInHall(5, 5)).to.be.equal("Unsuccessful change of seats in the hall.")
             expect(cinema.swapSeatsInHall('a', 'b')).to.be.equal("Unsuccessful change of seats in the hall.")
             expect(cinema.swapSeatsInHall(1, 'b')).to.be.equal("Unsuccessful change of seats in the hall.")
             expect(cinema.swapSeatsInHall('a', 1)).to.be.equal("Unsuccessful change of seats in the hall.")
+            expect(cinema.swapSeatsInHall(1)).to.be.equal("Unsuccessful change of seats in the hall.")
+            expect(cinema.swapSeatsInHall()).to.be.equal("Unsuccessful change of seats in the hall.")
         })
 
         it('should return correct price with valid param', () => {
             expect(cinema.swapSeatsInHall(1, 2)).to.be.equal('Successful change of seats in the hall.')
             expect(cinema.swapSeatsInHall(2, 1)).to.be.equal('Successful change of seats in the hall.')
+            expect(cinema.swapSeatsInHall(20, 1)).to.be.equal('Successful change of seats in the hall.')
+            expect(cinema.swapSeatsInHall(1, 20)).to.be.equal('Successful change of seats in the hall.')
         })
 
     })
